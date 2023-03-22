676a57aefde45e9e3141a6790434c72622fa6a7a
davidmoralesv <54856008+davidmoralesv@users.noreply.github.com>
2023-03-22 07:19:06 -0500

    add viewport and clean reports before execution adding pretest in scripts

--- cypress/e2e/fill-practiceform.cy.ts
+++ cypress/e2e/fill-practiceform.cy.ts
@@ -3,6 +3,9 @@ import { PracticeFormPage } from './pages/index'
 const practiceFormPage = new PracticeFormPage()
 
 describe('Fill form using const', () => {
+  before(() => {
+    cy.viewport(1920, 1080)
+  })
   it('Filling the form', function () {
     cy.visit('https://demoqa.com/automation-practice-form')
     practiceFormPage.fillForm()
